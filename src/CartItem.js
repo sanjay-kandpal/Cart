c24e64416fb0bfeb3f46ce6fc37882616db6955d
sanjay <72781686+sanjay-kandpal@users.noreply.github.com>
2022-07-11 20:31:56 +0530

    added increasequantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,12 +10,15 @@ class CartItem extends React.Component {
             img: ''
         }
     }
+    increaseQuantity= ()=>{
+        console.log('this.state',this.state);
+    }
     render() {
         const { price,title,qty} = this.state;
         return (
             <div className = "cart-item">
               <div className="left-block">
-                <img  style={styles.image}  />  
+                <img  style={styles.image} alt=""  />  
               </div>
               <div className="right-block">
                  <div style={{fontSize: 25}}>{title}</div>
@@ -23,7 +26,7 @@ class CartItem extends React.Component {
                  <div style={{color: '#777'}}>Qty:{qty}</div>
                  <div className="cart-item-actions">
                     {/* Buttons */}
-                    <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"/>
+                    <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" onClick={this.increaseQuantity}/>
                     <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1828/1828906.png"/>
                     <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1/1570.png"/>
                   </div>
